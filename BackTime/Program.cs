2cbb96bef0ada2b42065d03fee83799c11fb1ccf
lenol <lenol@TCOMPUTER>
2024-03-27 21:26:58 +0200

    Add project files.

---
+++ BackTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+Console.OutputEncoding = UTF8Encoding.UTF8;
+
+TimerCallback callback = new TimerCallback(TimerBack);
+Timer timer = new Timer(callback, null, 0, 1000);
+
+Console.WriteLine("Головний потік: Запуск");
+Thread.Sleep(10000);
+timer.Dispose();
+Console.WriteLine("Головний потік: Програма завершила роботу.");
+
+void TimerBack(object state)
+{
+    Console.WriteLine($"Callback: Потік викликаний на {DateTime.Now}");
+}
+
+
+
+
+/*
+1.Що таке багатонитковість?
+2.Загальні відомості про потоки:
+    1.що таке потік?
+    2.породження потоків;
+    3.призупинення, відновлення, припинення потоку;
+    4.пріоритети потоків.
+*/
---
+++ ThreadNET/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             3.Потоки в .NET Core:
+                1.простір System.Threading;
+                2.клас Thread;
+                3.потоки фонові і первинні;
+                4.породження потоків;
+                5.призупинення, відновлення, припинення потоку;
+                6.пріоритети потоків.
+             */
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Thread primaryThread = Thread.CurrentThread;
+            Console.WriteLine($"Основний потік: ID - {primaryThread.ManagedThreadId}, Назва - {primaryThread.Name}, Пріоритет - {primaryThread.Priority}");
+
+            // 3.потоки фонові і первинні;
+            Thread backgroundThread = new Thread(BackgroundThreadMethod);
+            backgroundThread.IsBackground = true;
+            backgroundThread.Start();
+
+            // 4.породження потоків;
+            Thread newThread = new Thread(NewThreadMethod);
+            newThread.Start();
+
+            // 5.призупинення, відновлення та припинення потоку.
+            Thread sleepThread = new Thread(SleepThreadMethod);
+            sleepThread.Start();
+
+            // 6.пріоритети потоків.
+            Thread priorityThread = new Thread(PriorityThreadMethod);
+            priorityThread.Priority = ThreadPriority.Highest;
+            priorityThread.Start();
+
+            Console.ReadLine();
+        }
+
+        static void BackgroundThreadMethod()
+        {
+            Console.WriteLine("Фоновий потік запущено.");
+            Thread.Sleep(5000);
+            Console.WriteLine("Фоновий потік завершено.");
+        }
+
+        static void NewThreadMethod()
+        {
+            Console.WriteLine("Новий потік запущено.");
+            Thread.Sleep(3000);
+            Console.WriteLine("Новий потік завершено.");
+        }
+
+        static void SleepThreadMethod()
+        {
+            Console.WriteLine("Потік для призупинення запущено.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Потік для призупинення завершено.");
+        }
+
+        static void PriorityThreadMethod()
+        {
+            Console.WriteLine("Потік з високим пріоритетом запущено.");
+            Thread.Sleep(4000);
+            Console.WriteLine("Потік з високим пріоритетом завершено.");
+        }
+    }
+}
